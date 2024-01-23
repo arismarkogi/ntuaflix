e373cbfeda084253f8e92bccdcec9991a74c0756
ntua-el20207 <el20207@mail.ntua.gr>
2024-01-23 16:00:10 +0200

    cli

--- cli-client/cli.js
+++ cli-client/cli.js
@@ -1,20 +1,55 @@
-// cli.js
-const api = require('./api');
+const axios = require('axios');
 
-const command = process.argv[2];
+const baseURL = 'https://{{host}}:9876/ntuaflix_api';
 
-switch (command) {
-  case 'getTitleById':
-    api.getTitleById(process.argv[3])
-      .then(response => console.log(response.data))
-      .catch(error => console.error(error));
-    break;
-  case 'searchTitle':
-    api.searchTitle(process.argv[3])
-      .then(response => console.log(response.data))
-      .catch(error => console.error(error));
-    break;
-  // Υπόλοιπες εντολές για τα υπόλοιπα endpoints
-  default:
-    console.log('Unknown command');
+// Ο κώδικας για να καλέσετε το API endpoint /title/:titleID
+async function getTitleById(titleID) {
+  try {
+    const response = await axios.get(`${baseURL}/title/${titleID}`);
+    // Εδώ μπορείτε να επεξεργαστείτε τα δεδομένα που έχετε λάβει από το back-end
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+async function searchTitleByPart(titlePart) {
+  try {
+    const response = await axios.get(`${baseURL}/searchtitle`);
+    // Εδώ μπορείτε να επεξεργαστείτε τα δεδομένα που έχετε λάβει από το back-end
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function searchByGenre(gquery) {
+  try {
+    const response = await axios.get(`${baseURL}/bygenre`);
+    // Εδώ μπορείτε να επεξεργαστείτε τα δεδομένα που έχετε λάβει από το back-end
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getNameById(nameID) {
+  try {
+    const response = await axios.get(`${baseURL}/name/${nameID}`);
+    // Εδώ μπορείτε να επεξεργαστείτε τα δεδομένα που έχετε λάβει από το back-end
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function searchNameByPart(namePart) {
+  try {
+    const response = await axios.get(`${baseURL}/searchname`);
+    // Εδώ μπορείτε να επεξεργαστείτε τα δεδομένα που έχετε λάβει από το back-end
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
